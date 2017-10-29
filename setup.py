7f113fcfcbe24bcbfbceac560cbdb7176fe3d15a
asdmueller <wessels@campus.tu-berlin.de>
2017-10-29 16:21:15 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='golden',
+      version='0.01',
+      description='',
+      url='http://github.com/timbo-slice/whats-golden',
+      author='timbo-slice',
+      author_email='',
+      license='MIT',
+      requires=['numpy', 'scipy'],
+      packages=['golden'],
+      zip_safe=False)
