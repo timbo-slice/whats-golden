c32feddf5ad6325a5101b73434394579c7d3d28a
asdmueller <wessels@campus.tu-berlin.de>
2017-10-30 15:57:31 +0100

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ gold cluster classification from stem images using machine learning
 ## references
 The code in golden/simulation.py is based on the method presented by D.S. He, Z.Y. Li and J. Yuan in
  "Kinematic HAADF-STEM image simulation of small nanoparticles", 2015
+
+## install
+you need numpy, scipy and matplotlib (ananconda)
+- clone the repo
+- ´´´python setup.py develop´´´
+- edit the config.py to point to your xyz files
--- golden/config.py
+++ golden/config.py
@@ -4,7 +4,7 @@ from pdb import set_trace  # imported here so it an be used throughout the modul
 import logging
 import sys
 
-BASEPATH = os.path.join(os.path.split(os.path.abspath(__file__))[0], '..')
+BASEPATH = os.path.join(os.path.split(os.path.abspath(__file__))[0], '..')  # basepath, i.e. basepath/golden/config.py
 CORDSPATH = os.path.join(BASEPATH, 'data/in/cords/')
 
 PICSPATH = os.path.join(BASEPATH, 'data/out/pics2/')
@@ -15,7 +15,7 @@ if not os.path.exists(PICSPATH):
 PIXELS = 512  # PIXELS x PIXELS image
 DEPTH_RESOLUTION = 25  # number of histogram bins in z-direction
 INTENSITY_DECAY = .015  # intensity is computed as 1 - intensity_decay * z-distance
-SIGMA = 5.  # the std of the gaussian convolution in pixels
+SIGMA = 5.  # the std of the gaussian convolution in pixels TODO change to absolute value in angstrom
 DEPTH_OFFSET = 25  # depth_offset = 0 means camera at the center of the specimen
 EXTRA_SIDE_PIXELS = 25  # Pixels added to the side to account for smearing of atoms at the edges of the image
 FFT_ZOOM_FACTOR = .38  # the fft images are restricted to the middle (low freq) part.
